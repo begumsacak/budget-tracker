a0aade5ace4e1fb369963966b02c506343367659
begumsacak <63175082+begumsacak@users.noreply.github.com>
2020-09-12 23:59:34 -0500

    service-worker.js

--- Develop/public/service-worker.js
+++ Develop/public/service-worker.js
@@ -1,3 +1,17 @@
+const FILES_TO_CACHE = [
+    '/',
+    '/index.html',
+    '/index.js',
+    '/manifest.webmanifest',
+    '/styles.css',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512',
+  ];
+  
+  const CACHE_NAME = "static-cache-v2";
+  const DATA_CACHE_NAME = "data-cache-v1";
+
+//
 // install
 self.addEventListener("install", function (evt) {
     // pre cache image data
@@ -15,3 +29,55 @@ self.addEventListener("install", function (evt) {
     self.skipWaiting();
   });
   
+  //Activate 
+
+  self.addEventListener("activate", function(evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
+      })
+    );
+  
+    self.clients.claim();
+  });
+
+  // Fetch
+
+  self.addEventListener("fetch", function(evt) {
+    // cache successful requests to the API
+    if (evt.request.url.includes("/api/")) {
+      evt.respondWith(
+        caches.open(DATA_CACHE_NAME).then(cache => {
+          return fetch(evt.request)
+            .then(response => {
+              // If the response was good, clone it and store it in the cache.
+              if (response.status === 200) {
+                cache.put(evt.request.url, response.clone());
+              }
+  
+              return response;
+            })
+            .catch(err => {
+              // Network request failed, try to get it from the cache.
+              return cache.match(evt.request);
+            });
+        }).catch(err => console.log(err))
+      );
+  
+      return;
+    }
+
+    // if the request is not for the API, serve static assets using "offline-first" approach.
+  evt.respondWith(
+    caches.match(evt.request).then(function(response) {
+      return response || fetch(evt.request);
+    })
+  );
+});
